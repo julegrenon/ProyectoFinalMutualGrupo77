8bce50b1e10d72198117471009b7a2bb037d5ea6
MariiiM <marinamereles@hotmail.com>
2023-10-02 16:49:49 -0300

    Clase orden

--- src/proyectomutual/entidades/Orden.java
+++ src/proyectomutual/entidades/Orden.java
@@ -1,14 +1,91 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectomutual.entidades;
 
-/**
- *
- * @author Alan
- */
+import java.time.LocalDate;
+
+
 public class Orden {
+    
+   
+    private int idOrden;
+    private LocalDate fecha;
+    private String formaPago;
+    private double importe;
+    private Afiliado idAfiliado;
+    private Prestador prestador;
+
+    public Orden() {
+    }
+
+    public Orden(LocalDate fecha, String formaPago, double importe, Afiliado afiliado, Prestador prestador) {
+        this.fecha = fecha;
+        this.formaPago = formaPago;
+        this.importe = importe;
+        this.idAfiliado = afiliado;
+        this.prestador = prestador;
+    }
+
+    public Orden(int idOrden, LocalDate fecha, String formaPago, double importe, Afiliado afiliado, Prestador prestador) {
+        this.idOrden = idOrden;
+        this.fecha = fecha;
+        this.formaPago = formaPago;
+        this.importe = importe;
+        this.idAfiliado = afiliado;
+        this.prestador = prestador;
+    }
+
+    public int getIdOrden() {
+        return idOrden;
+    }
+
+    public void setIdOrden(int idOrden) {
+        this.idOrden = idOrden;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    public Afiliado getAfiliado() {
+        return idAfiliado;
+    }
+
+    public void setAfiliado(Afiliado afiliado) {
+        this.idAfiliado = afiliado;
+    }
+
+    public Prestador getPrestador() {
+        return prestador;
+    }
+
+    public void setPrestador(Prestador prestador) {
+        this.prestador = prestador;
+    }
+
+    @Override
+    public String toString() {
+        return "Orden{" + "idOrden=" + idOrden + ", fecha=" + fecha + ", formaPago=" + formaPago + ", importe=" + importe + ", afiliado=" + idAfiliado + ", prestador=" + prestador + '}';
+    }
+     
     
 }
